8d76751da324ff96957a858e95f3b9749af74880
Bassem Ghali <bghali@unifonic.com>
2025-07-09 14:03:32 +0300

    fix redis version

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_elasticache_cluster" "redis-db" {
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = "default.redis6.x"
   engine_version       = "6.2"
   port                 = 6379
 
